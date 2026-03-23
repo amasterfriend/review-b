2640a72b984a70c7b609a1d5638cc1810797e5b6
shukvosky <920964205@qq.com>
2026-03-23 16:25:48 +0800

    B端实现rpc调用评价回复服务

--- internal/data/data.go
+++ internal/data/data.go
@@ -1,24 +1,50 @@
 package data
 
 import (
+	"context"
+	v1 "review-b/api/review/v1"
 	"review-b/internal/conf"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/validate"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/google/wire"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewBusinessRepo)
+var ProviderSet = wire.NewSet(NewReviewServiceClient, NewData, NewBusinessRepo)
 
 // Data .
 type Data struct {
 	// TODO wrapped database client
+	// 嵌入一个gRPC Client ，通过这个Client去调用review-service服务
+	rc  v1.ReviewClient
+	log *log.Helper
 }
 
 // NewData .
-func NewData(c *conf.Data) (*Data, func(), error) {
+func NewData(c *conf.Data, rc v1.ReviewClient, logger log.Logger) (*Data, func(), error) {
 	cleanup := func() {
 		log.Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	return &Data{rc: rc, log: log.NewHelper(logger)}, cleanup, nil
+}
+
+// NewReviewServiceClient 创建一个review-service的gRPC客户端
+func NewReviewServiceClient() v1.ReviewClient {
+	// import "github.com/go-kratos/kratos/v2/transport/grpc"
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("127.0.0.1:9000"),
+		// grpc.WithEndpoint("discovery:///review.service"),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+			validate.Validator(),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return v1.NewReviewClient(conn)
 }
